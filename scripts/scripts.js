edd126e3ff56d961116bbf8340adbe0e8766803f
Tarcísio Teles <tfst@poli.br>
2021-07-25 20:33:00 -0300

    unique selection of item per category is working (w/ parentNode)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,22 @@
 function markChecked(element) {
-   element.classList.toggle("selected-card");
-   //element.querySelector("ion-icon").style.display='initial';
-   element.querySelector("ion-icon").classList.toggle("hidden");
+   if (element.classList.contains("selected-card")) {
+      element.classList.remove("selected-card");
+      element.querySelector("ion-icon").classList.add("hidden");
+   }
+   else {
+      let father = element.parentNode;
+      let marcadoAntes = father.querySelector(".selected-card");
 
-}
+      if (marcadoAntes !== null) {
+         //contador--;
+         //comida = "";
+         //total -+ precoComida;
+         marcadoAntes.classList.remove("selected-card");
+         marcadoAntes.querySelector("ion-icon").classList.add("hidden");
+      }
+      //contador++;
+      element.classList.add("selected-card");
+      element.querySelector("ion-icon").classList.remove("hidden");
+   }
+
+   }
